07f9ebbfa47e2ebd7a04791406cd7c0bb821b86b
sberger94 <sberger94@gmail.com>
2022-03-18 14:46:39 -0700

    Defined DOM elements to update state

--- js/app.js
+++ js/app.js
@@ -3,8 +3,21 @@ const player1 = 'p1';
 const player2 = 'p2';
 
 // state
-let player1Token;
-let player2Token;
+let player1Token; // player 1's gamepiece
+let player2Token; // player 2's gamepiece
+let playerTurn; // Who's turn it is
+let player1Positions; // player 1's board positions
+let player2Positions; // player 2's board positions
+let winner;
+
+// DOM elements
+const columnBtns = document.querySelectorAll('.columnbtn');
+const player1Bank = document.querySelector('.player1');
+const player2Bank = document.querySelector('.player2');
+const playBtn = document.querySelector('#play-btn');
+const msgEl = document.querySelector('#msg');
+const boardEl = document.querySelector('.game-board');
+console.log(boardEl)
 
 // init
 
