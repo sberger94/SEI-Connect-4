931110a2cbd0daa9afa07e0bdf0726a845e87f92
sberger94 <sberger94@gmail.com>
2022-03-21 10:38:41 -0700

    Updated init function to clear the board, redefined currentBoard variable, declared turnAlternate function

--- js/app.js
+++ js/app.js
@@ -5,8 +5,7 @@ const playerId = [1,2];
 let player1Token; // player 1's gamepiece
 let player2Token; // player 2's gamepiece
 let playerTurn; // Who's turn it is
-let player1Positions; // player 1's board positions
-let player2Positions; // player 2's board positions
+let currentBoard // positions of both players
 let winner;
 
 // DOM elements
@@ -21,6 +20,8 @@ const boardEls = document.querySelectorAll('.game-board > div');
 const col1 = [boardEls[6],boardEls[3],boardEls[0]];
 const col2 = [boardEls[7],boardEls[4],boardEls[1]];
 const col3 = [boardEls[8],boardEls[5],boardEls[2]];
+const gameBoard = [col1, col2, col3];
+console.log(gameBoard)
 
 // init and event listeners
 playBtn.addEventListener('click', init);
@@ -35,6 +36,13 @@ function init(e){
     } else {
         msgEl.innerText = `${player2Bank.innerText} goes first!`
     };
+    gameBoard.forEach(column => {
+        column.forEach(element => element.className = 'empty-space');
+    });
+}
+
+function turnAlternate(){
+
 }
 
 // render
@@ -44,7 +52,12 @@ function render(){
 
 // button functions
 function handleClick(e){
-    console.log(e.target.tagName)
+    if(e.target.tagName === 'BUTTON'){
+        console.log(e.target.id);
+    } if(e.target.tagName === 'SECTION'){
+        return;
+    }
+    
 }
 
 // reset/replay
